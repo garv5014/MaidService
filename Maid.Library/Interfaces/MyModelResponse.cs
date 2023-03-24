de3835b57474b023ccbd29e22996657e00023cab
garv5014 <garv5014@gmail.com>
2023-03-24 15:23:40 -0600

    my model response

---
+++ Maid.Library/Interfaces/MyModelResponse.cs
@@ -0,0 +1,30 @@
+﻿using Postgrest.Models;
+using Postgrest.Responses;
+
+namespace Maid.Library.Interfaces;
+
+public class MyModelResponse<T> where T : BaseModel, new()
+{
+    public List<T> Models { get;  set; } = new List<T>();
+
+    public Postgrest.ClientOptions ClientOptons { get;  set; }
+    
+    public string? Content { get;  set; }
+
+    public HttpResponseMessage ResponseMessage { get;  set; }
+
+    public MyModelResponse()
+    {
+        
+    }
+    public MyModelResponse(ModeledResponse<T> modeledResponse)
+    {
+        Models = modeledResponse.Models;
+
+        ClientOptons = modeledResponse.ClientOptions;
+
+        Content = modeledResponse.Content;
+
+        ResponseMessage = modeledResponse.ResponseMessage;
+    }
+}
