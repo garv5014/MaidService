747691908b799e0b4639177b82c639f4c9a062f9
garv5014 <garv5014@gmail.com>
2023-03-27 19:22:29 -0600

    made navigation Interface and class

---
+++ MaidService/Services/NavigationService.cs
@@ -0,0 +1,16 @@
+﻿using Maid.Library.Interfaces;
+
+namespace MaidService.Services;
+
+public class NavigationService : INav
+{
+    public void NavigateTo(string path)
+    {
+        Shell.Current.GoToAsync(path);
+    }
+
+    public void NavigateToWithParameters(string path, Dictionary<string, object> args)
+    {
+        Shell.Current?.GoToAsync(path, args);
+    }
+}
