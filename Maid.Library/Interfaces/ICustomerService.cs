7d3cd2d8e331a16606d5e607d95604569e87433e
Snow-EWhitt <ethanwhittaker02@gmail.com>
2023-03-28 13:52:20 -0600

    Created CleaningAppoinment class.

--- Maid.Library/Interfaces/ICustomerService.cs
+++ Maid.Library/Interfaces/ICustomerService.cs
@@ -5,5 +5,8 @@ namespace Maid.Library.Interfaces;
 public interface ICustomerService
 {
     Task<CleaningContract> GetCleaningDetailsById(int contractId);
-    public Task<List<CleaningContract>> GetUpcomingAppointments();
+    public Task<IEnumerable<CleaningContract>> GetUpcomingAppointments();
+    public Task<IEnumerable<CleaningContract>> GetAppoinmentsByUserId();
+
+    // Option: Combine both functions to be one and pass in span of time.
 }
--- MaidService/ViewModels/CustomerScheduleViewModel.cs
+++ MaidService/ViewModels/CustomerScheduleViewModel.cs
@@ -1,11 +1,26 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Maid.Library.Interfaces;
+using MaidService.ComponentsViewModels;
+using MaidService.Services;
+using System.Collections.ObjectModel;
 
 namespace MaidService.ViewModels;
 
 public partial class CustomerScheduleViewModel : ObservableObject
 {
-    public CustomerScheduleViewModel()
+    private ICustomerService customerService;
+
+    public CustomerScheduleViewModel(ICustomerService customerService)
     {
-        
+        this.customerService = customerService;
+    }
+
+    public ObservableCollection<CleaningAppoinment> Appoinments { get; set; }
+
+    [RelayCommand]
+    public void Appear()
+    {
+        customerService
     }
 }
