32ca8a14f41590c2a12197e8b8070bfdab32f91f
garv5014 <garv5014@gmail.com>
2023-04-11 14:27:08 -0600

    redid some tables in the database and upgraded the dbmodels to match them

---
+++ Maid.Library/DbModels/CleanerAvailibilityModel.cs
@@ -0,0 +1,25 @@
+﻿using Postgrest.Attributes;
+using Postgrest.Models;
+
+namespace MaidService.Library.DbModels;
+[Table("cleaner_availability")]
+public class CleanerAvailabilityModel : BaseModel
+{
+    [PrimaryKey("id")]
+    public int Id { get; set; }
+
+    [Reference(typeof(ScheduleModel))]
+    public CleaningContractModel Schedule{ get; set; }
+
+    [Reference(typeof(CleanerModel))]
+    public CleanerModel Cleaner { get; set; }
+}
+
+public class CleanerAvailability
+{
+    public int Id { get; set; }
+
+    public Schedule Schedule { get; set; }
+
+    public Cleaner Cleaner { get; set; }
+}
