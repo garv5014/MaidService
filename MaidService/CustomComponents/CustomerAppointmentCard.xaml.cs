a1637f22dc166836177cb13fbd0896e472812a04
garv5014 <garv5014@gmail.com>
2023-04-12 12:29:01 -0600

    changed the name of the appoinmentCard

---
+++ MaidService/CustomComponents/CustomerAppointmentCard.xaml.cs
@@ -0,0 +1,11 @@
+using MaidService.ComponentsViewModels;
+
+namespace MaidService.CustomComponents;
+
+public partial class CustomerAppointmentCard : Frame
+{
+    public CustomerAppointmentCard()
+	{
+		InitializeComponent();
+	}
+}
